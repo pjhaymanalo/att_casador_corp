053283f931214c8c10e6cfc0b54003857e272ff8
pjhaymanalo <pjhaymanalo@gmail.com>
2025-05-20 13:21:42 -1200

    Trigger redeploy for Railway

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# Trigger redeploy for Railway
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
